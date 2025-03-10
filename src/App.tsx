40cfae64eb5c6a68d50316c1a4123fb8832706a1
BlurryQ <jazz.barlow@outlook.com>
2025-03-10 10:12:41 +0000

    Created Vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import logo from '/favicon.png';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <a href="/">
+        <img src={logo} className="logo" alt="Local Link logo" />
+      </a>
+    </>
+  );
+}
+
+export default App;
