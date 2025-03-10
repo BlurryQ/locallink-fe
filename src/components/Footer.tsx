dc395f4520d17f90d3729d80725bcb2d310d4e9c
BlurryQ <jazz.barlow@outlook.com>
2025-03-10 16:59:54 +0000

    Creates new components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import '../styles/footer.css';
+import logo from '../assets/logo.png';
+
+export default function Footer() {
+  return (
+    <div className="footer flex-between">
+      <ul>
+        Developer
+        <li>
+          <a href="https://github.com/BlurryQ">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/jazz-barlow/">LinkedIn</a>
+        </li>
+      </ul>
+      <ul>
+        Contact Us
+        <li>
+          <a href="tel:+440202 123 4567">0202 123 4567</a>
+        </li>
+        <li>
+          <a href="mailto:support@locallink.co.uk">support@locallink.co.uk</a>
+        </li>
+      </ul>
+      <a href="/">
+        <img src={logo} className="logo" alt="Local Link logo" />
+      </a>
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import '../styles/header.css';
+import Search from './Search';
+import logo from '../assets/logo.png';
+
+export default function Header() {
+  return (
+    <div className="flex-between header">
+      <a href="/">
+        <img src={logo} className="logo" alt="Local Link logo" />
+      </a>
+      <Search />
+      <ul className="flex-between w-10">
+        <li>
+          <a href="#">Sign Up</a>
+        </li>
+        <li>Theme</li>
+      </ul>
+    </div>
+  );
+}
