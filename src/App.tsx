ab8a3522958d354b7e3dd2a3c7b4a129d9955a6c
BlurryQ <jazz.barlow@outlook.com>
2025-03-11 10:43:23 +0000

    Implements React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+// components
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Signup from './components/auth/Signup';
+import Login from './components/auth/Login';
 
 function App() {
   return (
     <div className="app">
-      {/* header */}
       <Header />
+      <Routes>
+        <Route path="/" element={<h1>HOME</h1>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
 
-      {/* main */}
-
-      {/* footer */}
       <Footer />
     </div>
   );
