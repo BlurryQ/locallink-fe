6ebc895ce3b5074490e61c30f232918032e8cffa
BlurryQ <jazz.barlow@outlook.com>
2025-03-11 17:20:31 +0000

    Adds more routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
+import Events from './components/Events';
+import Event from './components/Event';
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path="/" element={<h1>HOME</h1>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/events/:id" element={<Event />} />
+        {/* add page not found */}
       </Routes>
 
       <Footer />
