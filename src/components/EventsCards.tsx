dd9f6f656b15ca46dd52e739183cdea581f79442
BlurryQ <jazz.barlow@outlook.com>
2025-03-13 19:01:24 +0000

    Refactoted code

--- src/components/EventsCards.tsx
+++ src/components/EventsCards.tsx
@@ -1,36 +1,26 @@
 import '../styles/eventsCard.css';
-import { format } from 'date-fns';
 import { EventType } from '../types/EventType';
 import logo from '../assets/logo.png';
+import formatEventTime from '../utils/formatEventTime';
+import capitalizeFirstLetterOfEachWord from '../utils/capitaliseFirstLetterOfEachWord';
 
 export default function EventsCard({ event }: { event: EventType }) {
-  // TODO move this to utils
-  const formatEventTime = (date: Date): string => {
-    const time = new Date(date);
-    const localTime = new Date(
-      time.getTime() + time.getTimezoneOffset() * 60 * 1000
-    );
-    return format(localTime, 'kk:mm EEE MMM do yyyy');
-  };
-
-  const start: string = formatEventTime(event.start);
-  const end: string = formatEventTime(event.end);
   const image: string = event.image_url === 'default' ? logo : event.image_url;
 
   return (
-    <a href={event.id}>
+    <a href={'/events/' + event.id}>
       <ul>
+        <li>{capitalizeFirstLetterOfEachWord(event.name)}</li>
+        <li>{formatEventTime(event.start)}</li>
+        {/* <li>End: {formatEventTime(event.end)}</li> */}
         <li>
-          <img src={image} alt="Event image" className="opaque" />
+          <img src={image} alt="Event image" className="placeholder" />
         </li>
-        <li>Name: {event.name}</li>
-        <li>Start: {start}</li>
-        <li>End: {end}</li>
         <ul className="location">
           Location:
-          <li>Name: {event.location.name}</li>
-          <li>Street: {event.location.street}</li>
-          <li>City: {event.location.city}</li>
+          <li>{event.location.name}</li>
+          <li>{event.location.street}</li>
+          <li>{event.location.city}</li>
         </ul>
       </ul>
     </a>
