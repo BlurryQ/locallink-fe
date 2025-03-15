15002166f1cef25a35c20e7800a70a67087aa089
BlurryQ <jazz.barlow@outlook.com>
2025-03-15 16:40:04 +0000

    Creates login function

---
+++ src/api/loginUser.ts
@@ -0,0 +1,19 @@
+import { comparePassword } from "../utils/Passwords";
+import baseURL from "./api";
+
+export default async function loginUser(email: string, password: string) {
+  // get user by email
+  try {
+    const res = await baseURL.get(`/users?email=` + email)
+    const hash = res.data.password
+    // compare passwords
+    const match: boolean = await comparePassword(hash, password)
+    const { id } = res.data
+    if (!match) return {error: "incorrect password"}
+    return {id, email}
+  } catch (error: any) {
+    if (error.response.statusText === "Not Found")
+      return {error: "email address not found"}
+    return {error}
+  }
+}
