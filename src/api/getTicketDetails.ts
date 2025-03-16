5eed0a3c3bd13ac3301c116c20c86d699f853989
BlurryQ <jazz.barlow@outlook.com>
2025-03-16 18:28:39 +0000

    Creates new API call

---
+++ src/api/getTicketDetails.ts
@@ -0,0 +1,6 @@
+import baseURL from "./api";
+
+export default async function getTicketDetails(ticketID: string) {
+  const response = await baseURL.get(`tickets/events/` + ticketID)
+  return response.data;
+}
