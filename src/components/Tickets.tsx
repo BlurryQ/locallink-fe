e317922bed62e1fcd6e1a3f54b904962b145511d
BlurryQ <jazz.barlow@outlook.com>
2025-03-17 19:50:02 +0000

    Finalises tickets component

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -1,28 +1,34 @@
 import '../styles/tickets.css';
-import { TicketType } from '../types/TicketType';
-import { useUser } from '../context/UserContext';
-import getTicketDetails from '../api/getTicketDetails';
-import TicketsCard from './TicketsCard';
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+// components
+import TicketsCard from './TicketsCard';
+
+// context
+import { useUser } from '../context/UserContext';
+
+// types
+import { TicketType } from '../types/TicketType';
+
+// utils
+import getTickets from '../utils/getTickets';
+import postEventToCalendar from '../utils/postEventToCalendar';
 
 export default function Tickets() {
+  const [searchParams] = useSearchParams();
   const [tickets, setTickets] = useState<TicketType[] | []>([]);
   const userContext = useUser();
   if (!userContext) return;
   const { user } = userContext;
 
   useEffect(() => {
-    const requestTickets = async (userID: string) => {
-      try {
-        const res = await getTicketDetails(userID);
-        setTickets(res.tickets);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
     if (!user) return;
-    else if (user.id) requestTickets(user.id);
+    else if (user.id) getTickets(user.id, setTickets);
+
+    // check if we have search param from Google callback
+    const code = searchParams.get('code');
+    if (code) postEventToCalendar(code);
   }, []);
 
   return (
