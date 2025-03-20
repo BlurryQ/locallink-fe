1749b5b8b3d8234db1c340772c5bc17bf914908e
BlurryQ <jazz.barlow@outlook.com>
2025-03-20 09:24:27 +0000

    Adds edit button if users events

--- src/components/EventsCards.tsx
+++ src/components/EventsCards.tsx
@@ -10,13 +10,29 @@ import { EventType } from '../types/EventType';
 import formatEventTime from '../utils/formatEventTime';
 import capitalizeFirstLetterOfEachWord from '../utils/capitaliseFirstLetterOfEachWord';
 
-export default function EventsCard({ event }: { event: EventType }) {
+export default function EventsCard({
+  event,
+  usersEvents = false,
+}: {
+  event: EventType;
+  usersEvents: boolean;
+}) {
   const image: string = event.image_url === 'default' ? logo : event.image_url;
   const eventPrice: string =
     event.price === 0 ? 'Free' : `£${(event.price / 100).toFixed(2)}`;
 
+  const editEvent = (e: any) => {
+    e.preventDefault();
+    window.location.href = '/events/edit/' + event.id;
+  };
+
   return (
     <a href={'/events/' + event.id}>
+      {usersEvents ? (
+        <button onClick={editEvent} className="edit">
+          Edit
+        </button>
+      ) : null}
       <ul>
         <li className="title">{capitalizeFirstLetterOfEachWord(event.name)}</li>
         <li>{formatEventTime(event.start)}</li>
