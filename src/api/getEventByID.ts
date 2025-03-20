7698db92a3c7cb418e6f10a7761adb851fc317e3
BlurryQ <jazz.barlow@outlook.com>
2025-03-20 09:20:49 +0000

    Consolidates  events API functions

--- src/api/getEventByID.ts
+++
@@ -1,8 +0,0 @@
-// api
-import baseURL from "./api";
-
-
-export default async function getEventByID(id: string) {
-  const response = await baseURL.get(`/events/` + id)
-  return response.data;
-}
--- src/api/getEvents.ts
+++
@@ -1,8 +0,0 @@
-// api
-import baseURL from "./api";
-
-
-export default async function getEvents() {
-  const response = await baseURL.get(`/events`)
-  return response.data;
-}
