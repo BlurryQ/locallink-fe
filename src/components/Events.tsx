9204eb332e17356f7bac1f83638047c9a7f29215
BlurryQ <jazz.barlow@outlook.com>
2025-03-20 09:24:06 +0000

    Adds filter for users created events

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -2,7 +2,7 @@ import '../styles/events.css';
 import { useEffect, useState } from 'react';
 
 // api
-import getEvents from '../api/getEvents';
+import { getEventByOrganiser, getEvents } from '../apis/events.api';
 
 // type
 import { EventType } from '../types/EventType';
@@ -11,17 +11,30 @@ import EventsCard from './EventsCards';
 // spinner
 import Lottie from 'lottie-react';
 import searching from '../assets/spinners/searching.json';
+import { useUser } from '../context/UserContext';
 
 export default function Events() {
   const [events, setEvents] = useState<EventType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const userContext = useUser();
+  if (!userContext) return;
+  const { user } = userContext;
+
+  const url: string = window.location.pathname;
+  const usersEventsOnly: boolean = url.split('/')[2] === 'mine';
 
   useEffect(() => {
     async function fetchEvents() {
+      if (!user.id) return;
       try {
         setLoading(true);
-        const data = await getEvents();
-        setEvents(data.events);
+        if (usersEventsOnly) {
+          const data = await getEventByOrganiser(user.id);
+          setEvents(data.events);
+        } else {
+          const data = await getEvents();
+          setEvents(data.events);
+        }
       } catch (err) {
         console.error(err);
       } finally {
@@ -40,11 +53,11 @@ export default function Events() {
         </div>
       ) : null}
       <div className="events-list">
-        {!loading && events.length > 1 ? (
+        {!loading && events.length > 0 ? (
           events.map((event: EventType) => {
             return (
               <div key={event.id} className="events-card">
-                <EventsCard event={event} />
+                <EventsCard event={event} usersEvents={usersEventsOnly} />
               </div>
             );
           })
