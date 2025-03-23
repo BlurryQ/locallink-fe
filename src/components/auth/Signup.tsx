37ce8c2928c22e157238e9f2f0a483d32c5d335c
BlurryQ <jazz.barlow@outlook.com>
2025-03-23 11:22:06 +0000

    Implements loaders

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -7,6 +7,10 @@ import { checkUserExists, postUser } from '../../apis/users.api';
 // package
 import PasswordChecklist from 'react-password-checklist';
 
+// spinner
+import Lottie from 'lottie-react';
+import thinking from '../../assets/spinners/thinking.json';
+
 // types
 import { UserType } from '../../types/UserType';
 
@@ -15,6 +19,7 @@ import { hashPassword } from '../../utils/Passwords';
 
 export default function Signup() {
   const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
   const [email, setEmail] = useState<string>('');
   let [password, setPassword] = useState<string>('');
   const [passwordConfirm, setPasswordConfirm] = useState<string>('');
@@ -25,7 +30,6 @@ export default function Signup() {
     try {
       const res = await postUser(user);
       console.log(res);
-      // TODO loader while posting and move function
       if (res.status) window.location.href = '/login';
     } catch (err) {
       console.error(err);
@@ -33,83 +37,99 @@ export default function Signup() {
   };
 
   const handleSubmit = async (e: any) => {
+    setLoading(true);
     e.preventDefault();
-    if (!passwordValid || !email) return;
+    if (!passwordValid || !email) return setLoading(false);
     const doesEmailExist: boolean | string = await checkUserExists(email);
     // if email address already exists
-    if (doesEmailExist) return setError('Email address already in database');
+    if (doesEmailExist) {
+      setLoading(false);
+      return setError('Email address already in database');
+    }
 
     // if database error
-    if (typeof doesEmailExist === 'string') return setError(doesEmailExist);
+    if (typeof doesEmailExist === 'string') {
+      setLoading(false);
+      return setError(doesEmailExist);
+    }
 
     password = await hashPassword(password);
     const newUser: UserType = {
       email,
       password,
     };
+    setLoading(false);
     createUser(newUser);
   };
 
   return (
-    <form className="signup" onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address <span className="mandatory">*</span>
-      </label>
-      <input
-        name="email"
-        type="email"
-        id="email"
-        placeholder="eg, someone@somewhere.com"
-        required
-        aria-required="true"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      ></input>
+    <>
+      {loading ? (
+        <div className="lottie-loader">
+          <Lottie animationData={thinking} loop={true} />
+        </div>
+      ) : null}
 
-      <label htmlFor="password">
-        Password <span className="mandatory">*</span>
-      </label>
-      <input
-        name="password"
-        type="password"
-        id="password"
-        placeholder="eg, Ex@mpl3!"
-        required
-        aria-required="true"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      ></input>
+      <form className="signup" onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email Address <span className="mandatory">*</span>
+        </label>
+        <input
+          name="email"
+          type="email"
+          id="email"
+          placeholder="eg, someone@somewhere.com"
+          required
+          aria-required="true"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        ></input>
 
-      <label htmlFor="pw-confirm">
-        Confirm Password <span className="mandatory">*</span>
-      </label>
-      <input
-        name="pw-confirm"
-        type="password"
-        id="pw-confirm"
-        placeholder="********"
-        required
-        aria-required="true"
-        onChange={(e) => setPasswordConfirm(e.target.value)}
-        value={passwordConfirm}
-      ></input>
+        <label htmlFor="password">
+          Password <span className="mandatory">*</span>
+        </label>
+        <input
+          name="password"
+          type="password"
+          id="password"
+          placeholder="eg, Ex@mpl3!"
+          required
+          aria-required="true"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        ></input>
 
-      <PasswordChecklist
-        rules={['minLength', 'specialChar', 'number', 'capital', 'match']}
-        minLength={8}
-        value={password}
-        valueAgain={passwordConfirm}
-        onChange={(isValid) => {
-          setPasswordValid(isValid);
-        }}
-      />
+        <label htmlFor="pw-confirm">
+          Confirm Password <span className="mandatory">*</span>
+        </label>
+        <input
+          name="pw-confirm"
+          type="password"
+          id="pw-confirm"
+          placeholder="********"
+          required
+          aria-required="true"
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          value={passwordConfirm}
+        ></input>
 
-      <button type="submit" className="submit">
-        Submit
-      </button>
-      <p className={`error ${error ? '' : 'invisible'}`}>
-        {error ? error : 'placeholder'}
-      </p>
-    </form>
+        <PasswordChecklist
+          rules={['minLength', 'specialChar', 'number', 'capital', 'match']}
+          minLength={8}
+          value={password}
+          valueAgain={passwordConfirm}
+          onChange={(isValid) => {
+            setPasswordValid(isValid);
+          }}
+        />
+
+        <button type="submit" className="submit">
+          Submit
+        </button>
+        <p className={`error ${error ? '' : 'invisible'}`}>
+          {error ? error : 'placeholder'}
+        </p>
+      </form>
+    </>
   );
 }
