24b3b3719106117d786ef840582c4b2d06458b8e
BlurryQ <jazz.barlow@outlook.com>
2025-03-23 16:08:04 +0000

    Updates baseURL to support multiple environments

--- src/apis/baseURL.ts
+++ src/apis/baseURL.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const urls =  ["http://localhost:5000", "https://locallink-be.onrender.com"]
+
 const baseURL = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: urls[1],
 });
 
 export default baseURL;
