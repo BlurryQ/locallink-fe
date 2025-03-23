bec5d11f5bf2502b6e0ef4271c876cd47ce0bf61
BlurryQ <jazz.barlow@outlook.com>
2025-03-23 12:27:53 +0000

    Implements Not Found component

--- src/components/EventForm.tsx
+++ src/components/EventForm.tsx
@@ -17,8 +17,10 @@ import { EventType } from '../types/EventType';
 import addOneHour from '../utils/addOneHour';
 import capitalizeFirstLetterOfEachWord from '../utils/capitaliseFirstLetterOfEachWord';
 import getImage from '../utils/getImage';
+import NotFound from './NotFound';
 
 export default function EventForm() {
+  const [eventNotFound, setEventNotFound] = useState<boolean>(false);
   const { eventID } = useParams();
   const currentTime: Date = new Date();
   const [eventName, setEventName] = useState<string>('');
@@ -54,8 +56,13 @@ export default function EventForm() {
   const { user } = userContext;
 
   const getEventData = async (id: string) => {
-    const event: EventType = await getEventByID(id);
-    if (event) setAllEventDetails(event);
+    try {
+      const event: EventType = await getEventByID(id);
+      if (event) setAllEventDetails(event);
+    } catch (err) {
+      console.error(err);
+      setEventNotFound(true);
+    }
   };
 
   const setAllEventDetails = (event: EventType) => {
@@ -75,11 +82,7 @@ export default function EventForm() {
 
   useEffect(() => {
     if (eventID) {
-      try {
-        getEventData(eventID);
-      } catch (err) {
-        console.error(err);
-      }
+      getEventData(eventID);
     }
   }, []);
 
@@ -129,160 +132,166 @@ export default function EventForm() {
   };
 
   return (
-    <div className="event-form">
-      <h3>{eventID ? 'Edit' : 'Create a new'} event</h3>
-      <form
-        id="eventForm"
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <img
-          src={getImage(eventCategory || 'default')}
-          alt={`${eventCategory} image`}
-        />
+    <>
+      {eventNotFound ? (
+        <NotFound />
+      ) : (
+        <div className="event-form">
+          <h3>{eventID ? 'Edit' : 'Create a new'} event</h3>
+          <form
+            id="eventForm"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <img
+              src={getImage(eventCategory || 'default')}
+              alt={`${eventCategory} image`}
+            />
 
-        <label htmlFor="name">Event Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          minLength={3}
-          maxLength={64}
-          onChange={(e) => setEventName(e.target.value)}
-          value={eventName}
-          required
-        />
+            <label htmlFor="name">Event Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              minLength={3}
+              maxLength={64}
+              onChange={(e) => setEventName(e.target.value)}
+              value={eventName}
+              required
+            />
 
-        <label htmlFor="start">Start Date and Time:</label>
-        <input
-          type="datetime-local"
-          id="start"
-          name="start"
-          onChange={(e) => {
-            const eventStartDate = new Date(e.target.value);
-            setEventStart(eventStartDate);
-            setEventEnd(addOneHour(eventStartDate));
-          }}
-          value={eventStart.toISOString().slice(0, 16)}
-          required
-        />
+            <label htmlFor="start">Start Date and Time:</label>
+            <input
+              type="datetime-local"
+              id="start"
+              name="start"
+              onChange={(e) => {
+                const eventStartDate = new Date(e.target.value);
+                setEventStart(eventStartDate);
+                setEventEnd(addOneHour(eventStartDate));
+              }}
+              value={eventStart.toISOString().slice(0, 16)}
+              required
+            />
 
-        <label htmlFor="end">End Date and Time:</label>
-        <input
-          type="datetime-local"
-          id="end"
-          name="end"
-          onChange={(e) => {
-            const eventEndDate = new Date(e.target.value);
-            setEventEnd(eventEndDate);
-          }}
-          value={eventEnd.toISOString().slice(0, 16)}
-          required
-        />
+            <label htmlFor="end">End Date and Time:</label>
+            <input
+              type="datetime-local"
+              id="end"
+              name="end"
+              onChange={(e) => {
+                const eventEndDate = new Date(e.target.value);
+                setEventEnd(eventEndDate);
+              }}
+              value={eventEnd.toISOString().slice(0, 16)}
+              required
+            />
 
-        <label htmlFor="location-name">Building Name:</label>
-        <input
-          type="text"
-          id="location-name"
-          name="location-name"
-          onChange={(e) => setEventLocationName(e.target.value)}
-          value={eventLocationName}
-        />
-        <label htmlFor="location-street">Street:</label>
-        <input
-          type="text"
-          id="location-street"
-          name="location-street"
-          onChange={(e) => setEventLocationStreet(e.target.value)}
-          value={eventLocationStreet}
-          required
-        />
-        <label htmlFor="location-city">City:</label>
-        <input
-          type="text"
-          id="location-city"
-          name="location-city"
-          onChange={(e) => setEventLocationCity(e.target.value)}
-          value={eventLocationCity}
-          required
-        />
-        <label htmlFor="location-postcode">Postcode/ Zip:</label>
-        <input
-          type="text"
-          id="location-postcode"
-          name="location-postcode"
-          onChange={(e) => setEventLocationPostcode(e.target.value)}
-          value={eventLocationPostcode}
-          required
-        />
-        <label htmlFor="location-country">Country:</label>
-        <input
-          type="text"
-          id="location-country"
-          name="location-country"
-          onChange={(e) => setEventLocationCountry(e.target.value)}
-          value={eventLocationCountry}
-          required
-        />
+            <label htmlFor="location-name">Building Name:</label>
+            <input
+              type="text"
+              id="location-name"
+              name="location-name"
+              onChange={(e) => setEventLocationName(e.target.value)}
+              value={eventLocationName}
+            />
+            <label htmlFor="location-street">Street:</label>
+            <input
+              type="text"
+              id="location-street"
+              name="location-street"
+              onChange={(e) => setEventLocationStreet(e.target.value)}
+              value={eventLocationStreet}
+              required
+            />
+            <label htmlFor="location-city">City:</label>
+            <input
+              type="text"
+              id="location-city"
+              name="location-city"
+              onChange={(e) => setEventLocationCity(e.target.value)}
+              value={eventLocationCity}
+              required
+            />
+            <label htmlFor="location-postcode">Postcode/ Zip:</label>
+            <input
+              type="text"
+              id="location-postcode"
+              name="location-postcode"
+              onChange={(e) => setEventLocationPostcode(e.target.value)}
+              value={eventLocationPostcode}
+              required
+            />
+            <label htmlFor="location-country">Country:</label>
+            <input
+              type="text"
+              id="location-country"
+              name="location-country"
+              onChange={(e) => setEventLocationCountry(e.target.value)}
+              value={eventLocationCountry}
+              required
+            />
 
-        <label htmlFor="capacity">Capacity:</label>
-        <input
-          type="number"
-          id="capacity"
-          name="capacity"
-          min={1}
-          onChange={(e) => setEventCapacity(Number(e.target.value))}
-          value={eventCapacity}
-          required
-        />
+            <label htmlFor="capacity">Capacity:</label>
+            <input
+              type="number"
+              id="capacity"
+              name="capacity"
+              min={1}
+              onChange={(e) => setEventCapacity(Number(e.target.value))}
+              value={eventCapacity}
+              required
+            />
 
-        <label htmlFor="details">Event Details:</label>
-        <textarea
-          id="details"
-          name="details"
-          maxLength={2048}
-          rows={5}
-          cols={25}
-          onChange={(e) => setEventDetails(e.target.value)}
-          value={eventDetails}
-          required
-        ></textarea>
+            <label htmlFor="details">Event Details:</label>
+            <textarea
+              id="details"
+              name="details"
+              maxLength={2048}
+              rows={5}
+              cols={25}
+              onChange={(e) => setEventDetails(e.target.value)}
+              value={eventDetails}
+              required
+            ></textarea>
 
-        <label htmlFor="price">Price (in pence):</label>
-        <input
-          type="number"
-          id="price"
-          name="price"
-          min={0}
-          onChange={(e) => setEventPrice(e.target.value)}
-          value={eventPrice}
-          required
-        />
+            <label htmlFor="price">Price (in pence):</label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              min={0}
+              onChange={(e) => setEventPrice(e.target.value)}
+              value={eventPrice}
+              required
+            />
 
-        <label htmlFor="category">Category:</label>
-        <select
-          id="category"
-          name="category"
-          onChange={(e) => setEventCategory(e.target.value)}
-          value={eventCategory}
-          required
-        >
-          <option value="" disabled>
-            Please select a category
-          </option>
-          {eventCategories.map((category) => (
-            <option key={category} value={category}>
-              {capitalizeFirstLetterOfEachWord(category)}
-            </option>
-          ))}
-        </select>
+            <label htmlFor="category">Category:</label>
+            <select
+              id="category"
+              name="category"
+              onChange={(e) => setEventCategory(e.target.value)}
+              value={eventCategory}
+              required
+            >
+              <option value="" disabled>
+                Please select a category
+              </option>
+              {eventCategories.map((category) => (
+                <option key={category} value={category}>
+                  {capitalizeFirstLetterOfEachWord(category)}
+                </option>
+              ))}
+            </select>
 
-        <button type="submit" className="create-event">
-          {eventID ? 'Edit Event' : 'Create Event'}
-        </button>
-      </form>
-    </div>
+            <button type="submit" className="create-event">
+              {eventID ? 'Edit Event' : 'Create Event'}
+            </button>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
